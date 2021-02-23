228b850c7b6505c44b0ddfa8824d4e975f27ab8f
kecarman74 <kecarman74@gmail.com>
2021-02-22 19:56:02 -0800

    updated app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import dash
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
 server = app.server
